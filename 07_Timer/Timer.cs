91e4972884fc8ac96c74abe946ea212f7105508a
davi7725 <davi7725@edu.eal.dk>
2016-09-20 11:13:27 +0200

    Add project files.

---
+++ 07_Timer/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _07_Timer
+{
+    internal class Timer
+    {
+        public int Seconds { get; set; }
+
+        public override string ToString()
+        {
+            string sec = "";
+            int minutes = Seconds / 60;
+            int hour = minutes/60;
+            
+            if(Seconds > 60)
+            {
+                Seconds = Seconds - 60*minutes;
+            }
+            
+            if(minutes > 60)
+            {
+                minutes = minutes - 60;
+            }
+
+            sec = Padded(Seconds);
+
+            return "0" +hour + ":0" + minutes + ":" + sec;
+        }
+
+        internal string Padded(int v)
+        {
+            string returnValue = "";
+            if (v < 10)
+            {
+                returnValue = "0" + v;
+            }
+            else
+            {
+                returnValue = v.ToString();
+            }
+            return returnValue;
+        }
+    }
+}
